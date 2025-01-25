029c5096a1f3160142046318d1b7db16f02f942e
Abbas <abbeekhan786@gmail.com>
2025-01-25 18:10:21 +0500

    initial

--- src/main/java/com/attendancemanagementsystem/repository/DepartmentRepo.java
+++ src/main/java/com/attendancemanagementsystem/repository/DepartmentRepo.java
@@ -1,4 +1,9 @@
 package com.attendancemanagementsystem.repository;
 
-public interface DepartmentRepo {
+import com.attendancemanagementsystem.model.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepo extends JpaRepository<Department, Long> {
 }
