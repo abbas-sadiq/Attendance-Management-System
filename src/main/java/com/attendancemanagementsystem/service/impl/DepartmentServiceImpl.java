bd8d2023f5e10d0baae623f275a0819a4faebbd0
Abbas <abbeekhan786@gmail.com>
2025-01-25 18:13:18 +0500

    initial....

--- src/main/java/com/attendancemanagementsystem/service/impl/DepartmentServiceImpl.java
+++ src/main/java/com/attendancemanagementsystem/service/impl/DepartmentServiceImpl.java
@@ -1,4 +1,45 @@
 package com.attendancemanagementsystem.service.impl;
 
-public class DepartmentServiceImpl {
+import com.attendancemanagementsystem.model.Department;
+import com.attendancemanagementsystem.repository.DepartmentRepo;
+import com.attendancemanagementsystem.service.DepartmentService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DepartmentServiceImpl implements DepartmentService {
+
+    private final DepartmentRepo departmentRepo;
+
+    public DepartmentServiceImpl(DepartmentRepo departmentRepo) {
+        this.departmentRepo = departmentRepo;
+    }
+
+    @Override
+    public Department createDepartment(Department department) {
+        return departmentRepo.save(department);
+    }
+
+    @Override
+    public List<Department> getAllDepartments() {
+        return departmentRepo.findAll();
+    }
+
+    @Override
+    public Department getDepartmentById(Long id) {
+        return departmentRepo.findById(id).orElseThrow(() -> new RuntimeException("Department not found"));
+    }
+
+    @Override
+    public Department updateDepartment(Long id, Department department) {
+        Department existingDepartment = getDepartmentById(id);
+        existingDepartment.setDepartmentName(department.getDepartmentName());
+        return departmentRepo.save(existingDepartment);
+    }
+
+    @Override
+    public void deleteDepartment(Long id) {
+        departmentRepo.deleteById(id);
+    }
 }
