0197a11253a3103a18aa3169f67d6f93bfa8d073
Abbas <abbeekhan786@gmail.com>
2025-01-25 18:12:15 +0500

    initial....

--- src/main/java/com/attendancemanagementsystem/service/DepartmentService.java
+++ src/main/java/com/attendancemanagementsystem/service/DepartmentService.java
@@ -1,4 +1,13 @@
 package com.attendancemanagementsystem.service;
 
+import com.attendancemanagementsystem.model.Department;
+
+import java.util.List;
+
 public interface DepartmentService {
+    Department createDepartment(Department department);
+    List<Department> getAllDepartments();
+    Department getDepartmentById(Long id);
+    Department updateDepartment(Long id, Department department);
+    void deleteDepartment(Long id);
 }
