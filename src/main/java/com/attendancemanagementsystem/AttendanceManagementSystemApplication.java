6f4e7ccaf75507d5f408a1ef46e97f950f2b7f53
Abbas <abbeekhan786@gmail.com>
2025-01-28 15:57:24 +0500

    initial....

--- src/main/java/com/attendancemanagementsystem/AttendanceManagementSystemApplication.java
+++ src/main/java/com/attendancemanagementsystem/AttendanceManagementSystemApplication.java
@@ -2,7 +2,7 @@ package com.attendancemanagementsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Spring boot Application
 @SpringBootApplication
 public class AttendanceManagementSystemApplication {
 
